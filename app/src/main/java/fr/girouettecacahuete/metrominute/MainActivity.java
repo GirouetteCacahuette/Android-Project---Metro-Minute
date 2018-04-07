a362f586ffb703e039cb810cab7676d4f07082c6
Martin Prieur <martin.prieur@outlook.fr>
2018-04-07 13:36:36 +0200

    Ajout activité liste métros

--- app/src/main/java/fr/girouettecacahuete/metrominute/MainActivity.java
+++ app/src/main/java/fr/girouettecacahuete/metrominute/MainActivity.java
@@ -2,15 +2,36 @@ package fr.girouettecacahuete.metrominute;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+    private Button rechercher_Button = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rechercher_Button = findViewById(R.id.RButton);
+        rechercher_Button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId())
+        {
+            case R.id.RButton:
+
+                break;
+
+            case R.id.MlistButton:
+
+                break;
+        }
 
     }
 }
---
+++ app/src/main/java/fr/girouettecacahuete/metrominute/MetroListActivity.java
@@ -0,0 +1,15 @@
+package fr.girouettecacahuete.metrominute;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MetroListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.metro_list);
+
+
+    }
+}
