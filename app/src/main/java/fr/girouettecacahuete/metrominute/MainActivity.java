803a047fe4e90f47ce78e2b347adb4c32cc2d3bb
Martin Prieur <martin.prieur@outlook.fr>
2018-04-07 11:09:14 +0200

    Ajout bcakground

--- app/src/main/java/fr/girouettecacahuete/metrominute/MainActivity.java
+++ app/src/main/java/fr/girouettecacahuete/metrominute/MainActivity.java
@@ -5,13 +5,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    TextView titre = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        titre = findViewById(R.id.titre);
+
     }
 }
